73d8fb2a2ccd2ac204dbfeb17b62734ec802ae1d
reggev <reggev.am@gmail.com>
2019-08-22 23:16:49 +0300

    upadted z-offset to align the probe better

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -774,7 +774,7 @@
  *    (0,0)
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER -36  // X offset: -left  +right  [of the nozzle]
-#define Y_PROBE_OFFSET_FROM_EXTRUDER -5  // Y offset: -front +behind [the nozzle]
+#define Y_PROBE_OFFSET_FROM_EXTRUDER -40  // Y offset: -front +behind [the nozzle]
 #define Z_PROBE_OFFSET_FROM_EXTRUDER 0   // Z offset: -below +above  [the nozzle]
 
 // Certain types of probes need to stay away from edges
